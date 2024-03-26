44d1e3ce88cdeaf81437b1725a385299c2f8d07d
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-03-26 11:38:39 +0530

    Staking_Daap

---
+++ Staking_Daap/src/Components/Wallet/Wallet.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+export default function Wallet() {
+    const [state, setState] = useState({
+        provider:null,
+        account:null,
+        stakingContrat :null,
+        stakingTokenContract : null,
+        chainId : null
+    })
+  return (
+    <>
+    
+    </>
+  )
+}
