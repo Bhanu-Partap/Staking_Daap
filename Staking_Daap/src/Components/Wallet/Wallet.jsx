9976c7ef083d8555d56c7e992e6960638b0cc8f0
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-03-26 11:57:14 +0530

    wallet

--- Staking_Daap/src/Components/Wallet/Wallet.jsx
+++ Staking_Daap/src/Components/Wallet/Wallet.jsx
@@ -1,16 +1,29 @@
 import { useState } from 'react'
 
-export default function Wallet() {
+const Wallet = ()=>{
     const [state, setState] = useState({
-        provider:null,
-        account:null,
-        stakingContrat :null,
+        provider : null,
+        account:  null,
+        stakingContract :  null,
         stakingTokenContract : null,
-        chainId : null
+        chainId :null 
     })
-  return (
-    <>
-    
-    </>
-  )
+
+    const [isLoading, setIsLoading]= useState(false)
+
+    const handleWallet =async()=>{
+        try{
+            setIsLoading(true)
+            const {provider,account,stakingContract,stakingTokenContract,chainId} = await connectWallet();
+            setState({provider,account,stakingContract,stakingTokenContract,chainId});
+        }
+        catch(error){
+            console.error("Wallet Not Connected", error.message);
+        }
+        finally{
+            setIsLoading(false)
+        }
+    }
+
 }
+export default Wallet;
