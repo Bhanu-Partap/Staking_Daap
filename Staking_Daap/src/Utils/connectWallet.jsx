45015361c74f9f352bda61528f57e0dd2c09a65a
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-03-26 12:49:59 +0530

    wallet  connection setup

--- Staking_Daap/src/Utils/connectWallet.jsx
+++ Staking_Daap/src/Utils/connectWallet.jsx
@@ -2,15 +2,31 @@ import {ethers} from 'ethers'
 import stakingAbi from "../ABI/stakingAbi.json"
 import stakingTokenAbi from "../ABI/stakeTokenAbi.json"
 
-export default function connectWallet =async()=> {
+export const connectWallet =  async()=> {
   try{
     let [signer,provider,stakingContract, stakingTokenContract, chainId] = [null];
     if(window.ethereum === null){
         throw new Error("Metamask is not installed")
     }
     const account = await window.ethereum.request({
-        method:"eth_requestAccounts"
+        method:'eth_requestAccounts'
     })
+
+    let chainIdHex = await window.ethereum.request({
+        method:'eth_chainId'
+    }) 
+    chainId = parseInt(chainIdHex,10)
+    let selectedAccount = account[0]
+    if(!selectedAccount){
+        throw new Error("No ethereum account available")
+    }
+
+    provider = new ethers.BrowserProvider(window.ethereum)
+    signer = await provider.getSigners()
+
+    // Creating Instance
+    
+
   }
   catch(error){
     console.error(error);
